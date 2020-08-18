2214ed4dc95f7ac7376e31e338b7e59985e45302
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 16:28:14 -0700

    php get

---
+++ allantan.site/cgi-bin/php-get-echo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GET Echo</title>
+</head>
+<body>
+  <?php
+    $qString = $_SERVER['QUERY_STRING'];
+    echo "Raw query string: " . $qString;
+    echo "<br />"
+    echo "Formatted Query String: "
+    parse_str($qString, $result);
+    foreach($result as $item => $item_val) {
+      echo $item . ": " . $item_val;
+    }
+  ?>
+</body>
+</html>

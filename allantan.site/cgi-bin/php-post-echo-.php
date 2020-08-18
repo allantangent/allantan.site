c5fcab739d91578f4bf7bb7baf6b7a547e2db60a
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 16:43:16 -0700

    php post

--- allantan.site/cgi-bin/php-post-echo-.php
+++ allantan.site/cgi-bin/php-post-echo-.php
@@ -8,21 +8,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>GET Echo</title>
+  <title>POST Message Body</title>
 </head>
 <body>
   <header>
-    <h1>GET query string</h1>
+    <h1>POST Message Body</h1>
   </header>
   <hr />
+
+  <p>Message Body: </p>
   <?php
-    $qString = $_SERVER['QUERY_STRING'];
-    echo "Raw query string: " . $qString;
-    echo "<br />";
-    echo "Formatted Query String: ";
-    echo "<br />";
-    parse_str($qString, $result);
-    foreach($result as $item => $item_val) {
+    foreach($_POST as $item => $item_val) {
       if($item_val) {
         echo $item . ": " . $item_val;
         echo "<br />";

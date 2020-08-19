f3a96137e8459279fdfd697bcb33f9d26d9869ed
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 19:46:47 -0700

    python env

--- allantan.site/cgi-bin/py-env.py
+++ allantan.site/cgi-bin/py-env.py
@@ -3,5 +3,15 @@ import cgi
 import os
 
 print('Content-type: text/html\r\n\r\n')
+print ('<html>')
+print ('<head>')
+print ('<title>Environment Variables</title>')
+print ('</head>')
+print ('<body>')
+print ('<h1>Environment Variables</h1>')
+print ('<hr />')
 for param in os.environ.keys():
   print('<b>%s</b>: %s</br>' % (param, os.environ[param]))
+
+print ('</body>')
+print ('</html>')

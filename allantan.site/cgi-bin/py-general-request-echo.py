20eb6ab164d405d32a329641af5a9d2fb4a3b511
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 20:38:01 -0700

    python general

--- allantan.site/cgi-bin/py-general-request-echo.py
+++ allantan.site/cgi-bin/py-general-request-echo.py
@@ -17,7 +17,7 @@ print ('<p><b>Query String:</b> ' + os.environ['QUERY_STRING'] + '</p>')
 
 qString = cgi.FieldStorage()
 print ('<p><b>Message Body:</b> ' +  '</p>')
-if qString:
+if bool(qString):
   for key in qString:
     if key is not None:
       print('<p>%s: %s</p>' % (key, qString[key].value))

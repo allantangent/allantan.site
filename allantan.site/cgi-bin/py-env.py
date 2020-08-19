a635d4cb9d3b9e5b6eaff0bcde71a1fdeec7ee47
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 19:43:02 -0700

    python env

--- allantan.site/cgi-bin/py-env.py
+++ allantan.site/cgi-bin/py-env.py
@@ -4,4 +4,4 @@ import os
 
 print('Content-type: text/html\r\n\r\n')
 for param in os.environ.keys():
-  print('<b>%s</b>: %s<\br>' % (param, os.environ[param]))
+  print('<b>%s</b>: %s</br>' % (param, os.environ[param]))

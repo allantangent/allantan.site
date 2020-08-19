a700bfb5ae394774eeb27d5006904c196764fbf4
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 20:23:50 -0700

    python general

---
+++ allantan.site/cgi-bin/py-general-request-echo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import cgi
+import os
+
+print('Cache-Control: no-cache')
+print('Content-type: text/html\r\n\r\n')
+print ('<html>')
+print ('<head>')
+print ('<title>General Echo</title>')
+print ('</head>')
+print ('<body>')
+print ('<h1>General Request Echo</h1>')
+print ('<hr />')
+print ('<p><b>HTTP Protocol:</b> ' + os.environ['SERVER_PROTOCOL'] + '</p>')
+print ('<p><b>HTTP Method:</b> ' + os.environ['REQUEST_METHOD'] + '</p>')
+print ('<p><b>Query String:</b> ' + os.environ['QUERY_STRING'] + '</p>')
+
+qString = cgi.FieldStorage()
+print ('<p><b>Message Body:</b> ' +  '</p>')
+for key in qString:
+  if key:
+    print('<p>%s: %s</p>' % (key, qString[key].value))
+
+print ('</body>')
+print ('</html>')

84f92ac7cedb4b3db8e131dbff7554c3e071cf61
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 19:33:51 -0700

    python json

--- allantan.site/cgi-bin/py-hello-json-world.py
+++ allantan.site/cgi-bin/py-hello-json-world.py
@@ -2,16 +2,17 @@
 from datetime import datetime
 import cgi
 import os
+import json
 
+data_set = {"message": "Hello World", "date": datetime.now(), "currentIp": cgi.escape(os.environ["REMOTE_ADDR"])}
 print ("Content-type:text/html\r\n\r\n")
 print ('<html>')
 print ('<head>')
 print ('<title>Hello, Python!</title>')
 print ('</head>')
 print ('<body>')
-print ('<h1>Hello, Python!</h1>')
-print ('<p>Current time: ', datetime.now())
-print ('</p>')
-print ('<p>Your IP address is: ' + cgi.escape(os.environ["REMOTE_ADDR"]) + '</p>')
+print ('<pre>')
+print (json.dumps(data_set))
+print ('</pre>')
 print ('</body>')
 print ('</html>')

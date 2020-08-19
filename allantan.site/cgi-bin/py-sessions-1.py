ef2eefcbf7955baee31693388969a11139a5707c
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-18 21:15:25 -0700

    python session

---
+++ allantan.site/cgi-bin/py-sessions-1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import cgi
+import os
+
+name = "You do not have a name set"
+print('Cache-Control: no-cache;')
+if(not inSession() and cgi.FieldStorage()['username'].value is not None):
+  print('Set-Cookie:session = ' + cgi.FieldStorage()['username'].value)
+
+print('Content-type: text/html\r\n\r\n')
+print ('<html>')
+print ('<head>')
+print ('<title>General Echo</title>')
+print ('</head>')
+print ('<body>')
+print ('<h1>Python Sessions Page 1</h1>')
+
+name = cgi.FieldStorage()['username'].value
+if name is None:
+  name = 'You do not have a name set'
+
+print('<p><b>Name:</b> %s' % name)
+
+print ('<a href=\'./py-sessions-2.py\'>Session Page 2</a><br/>')
+print ('<a href=\'../py-cgiform.html\'>Python CGI Form</a><br />')
+print ('<form style=\'margin-top:30px\' action=\'./py-destroy-session.py\' method=\'get\'>')
+print ('<button type=\'submit\'>Destroy Session</button>')
+print ('</form>')
+
+print ('</body>')
+print ('</html>')
+
+def inSession():
+  if os.environ.has_key('HTTP_COOKIE'):
+   for cookie in map(strip, split(os.environ['HTTP_COOKIE'], ';')):
+      (key, value ) = split(cookie, '=')
+      if key == "session":
+         val = value
+    if val > 0:
+      return True
+  return False

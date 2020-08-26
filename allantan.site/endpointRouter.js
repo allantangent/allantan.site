465c12f72184f31ebb4d86255ec9c450970531d4
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-26 12:51:33 -0700

    adding specific get

--- allantan.site/endpointRouter.js
+++ allantan.site/endpointRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectID;
 
 // init mongodb
 const MongoClient = mongodb.MongoClient;

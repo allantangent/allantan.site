9f96754a50cc6b412a71e11774a516682344dda8
allantangent <43974058+allantangent@users.noreply.github.com>
2020-08-26 20:12:04 -0700

    edit helloviz

--- allantan.site/index.js
+++ allantan.site/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 	next();

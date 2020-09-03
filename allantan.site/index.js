0b2d5e011162958c9c549d5002e04d844460e8dc
allantangent <43974058+allantangent@users.noreply.github.com>
2020-09-03 04:28:16 -0700

    start hw4

--- allantan.site/index.js
+++ allantan.site/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 	next();
 });

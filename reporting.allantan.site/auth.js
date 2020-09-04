5ea9d79bd5b1ab856ce9d1febec9a2ed5e54ac6a
allantangent <43974058+allantangent@users.noreply.github.com>
2020-09-04 04:07:48 -0700

    auth

--- reporting.allantan.site/auth.js
+++ reporting.allantan.site/auth.js
@@ -3,15 +3,15 @@ const session = require("express-session");
 const body    = require("body-parser");
 const app = express();
 const port = 3003;
- 
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
- 
+
 var myUsers = [
     { id: 1, username: 'admin', password: 'admin', email: 'admin@example.com' },
     { id: 2, username: 'michael', password: 'password', email: 'mborkows@ucsd.edu' }
 ];
- 
+
 function findUser(username, func) {
     for ( let user of myUsers ) {
         if ( user.username === username ) {
@@ -20,7 +20,7 @@ function findUser(username, func) {
     }
     return func(null, null);
 }
- 
+
 passport.serializeUser(function(user, done) {
     done(null, user.username);
 });
@@ -29,7 +29,7 @@ passport.deserializeUser(function(username, done) {
         done(err, user);
     });
 });
- 
+
 passport.use( new LocalStrategy({ usernameField: 'username',
                                   passwordField: 'password' },
     function(username, password, done) {
@@ -44,21 +44,21 @@ passport.use( new LocalStrategy({ usernameField: 'username',
         });
     }
 ));
- 
+
 app.use(express.json());
 app.use(session({ secret: 'nyan cat', resave: false, 
                   saveUninitialized: false }));
 app.use(body());
 app.use(passport.initialize());
 app.use(passport.session());
- 
+
 function ensureAuthenticated(req, res, next) {
     if ( req.isAuthenticated() ) { return next(); }
     res.redirect('login');
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////
- 
+
 // Public side: Login page
 app.get('/login', function(req, res) {
     res.sendFile('public_html/login.html', { root: __dirname });
@@ -66,16 +66,16 @@ app.get('/login', function(req, res) {
 app.post('/login', passport.authenticate('local', 
     { successRedirect: 'home', failureRedirect: 'login'}
 ));
- 
+
 // In between: Logout
 app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('login');
 });
- 
+
 // Private side: Home page
 app.get('/home', ensureAuthenticated, function(req, res) {
-        res.sendFile('../index.html', { root: __dirname });
+        res.sendFile('index.html', { root: __dirname });
 });
- 
+
 app.listen(port);
